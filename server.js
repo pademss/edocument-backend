3ff42c9d6cda6206b600f664137297311407304d
pademss <fatma.putri@ui.ac.id>
2022-01-30 21:37:46 +0700

    header lagi

--- server.js
+++ server.js
@@ -9,10 +9,19 @@ app.use(express.json()); // parse json bodies in the request object
 // Redirect requests to endpoint starting with /posts to postRoutes.js
 app.use("/buku", require("./routes/bookRoutes"));
 
+// app.use(function (req, res, next) {
+//   //set headers to allow cross origin requestt
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
+
 app.use(function (req, res, next) {
-  //set headers to allow cross origin requestt
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
