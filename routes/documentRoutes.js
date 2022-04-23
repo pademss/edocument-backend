47ae5e5802250f7a59730eb387da2d2b5fde2b20
pademss <fatma.putri@ui.ac.id>
2022-04-23 16:54:00 +0700

    benerin routes

--- routes/documentRoutes.js
+++ routes/documentRoutes.js
@@ -2,7 +2,6 @@ const router = require("express").Router();
 const controller = require("../controllers/documentController");
 
 router.route("/").get(controller.getAllDocument).post(controller.addDocument);
-router.route("/upload").post(controller.uploadDocument);
 router
   .route("/:id_dokumen")
   .get(controller.getDocumentById)
